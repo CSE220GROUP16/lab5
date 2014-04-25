03aacde1f0353206a1bf1cb725962ad52d1e281a
Jie <jietan25@gmail.com>
2014-04-25 13:07:17 -0700

    version 1 orginal files lab 5

---
+++ Literal.h
@@ -0,0 +1,19 @@
+#ifndef __Lab5__Literal__
+#define __Lab5__Literal__
+
+
+#include "Token.h"
+#include "common.h"
+template<class T>
+class Literal : public Token
+{
+  private:
+    T literal;
+  public:
+    Literal(T lit);
+    ~Literal();
+    void setLiteral(T lit);
+    T getLiteral();
+    virtual void print();
+};
+#endif
